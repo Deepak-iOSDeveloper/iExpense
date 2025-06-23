af5e397b8d13d8c6a3634d02bd09b1691b0edec8
deepak <deepakjgrt99@gmail.com>
2025-06-23 17:47:54 +0530

    Initial commit - it's a expense tracker app in which we can separate our expense with personal and business label

---
+++ iExpense/AddExpense.swift
@@ -0,0 +1,18 @@
+//
+//  AddExpense.swift
+//  iExpense
+//
+//  Created by Deepak Kumar Behera on 04/06/25.
+//
+
+import SwiftUI
+
+struct AddExpense: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddExpense()
+}
---
+++ iExpense/OverAllView.swift
@@ -0,0 +1,18 @@
+//
+//  OverAllView.swift
+//  iExpense
+//
+//  Created by Deepak Kumar Behera on 05/06/25.
+//
+
+import SwiftUI
+
+struct OverAllView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OverAllView()
+}
